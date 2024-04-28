b6a578f1e94b39122a3dab002cfef705fd15b93f
Gogtam8 <129152884+Gogtam8@users.noreply.github.com>
2024-04-28 21:50:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import subprocess
 def main():
     while True:
         print("Menu:")
-        print("1. Option 1")
-        print("2. Option 2")
-        print("3. Scan et vulnérabilité")
+        print("1. Scan et vulnérabilité")
+        print("2. Enumération")
+        print("3. Option 3")
         print("4. Quitter")
         choice = input("Choisissez une option : ")
 
         if choice == '1':
-            print("Vous avez choisi l'option 1")
-            # Ajoutez ici le code pour l'option 1
-
-        elif choice == '2':
-            print("Vous avez choisi l'option 2")
-            # Ajoutez ici le code pour l'option 2
-
-        elif choice == '3':
             print("Vous avez choisi de lancer le scan et la vérification des vulnérabilités.")
             subprocess.run(["python", "test.py"])  # Exécute le script test.py
+
+        elif choice == '2':
+            print("Vous avez choisi de lancer l'enumération des repertoires.")
+            subprocess.run(["python", "repertoires.py"])  # Exécute le script repertoires.py
+
+        elif choice == '3':
+            print("VVous avez choisi l'option 3")
+       
 
         elif choice == '4':
             print("Au revoir !")
