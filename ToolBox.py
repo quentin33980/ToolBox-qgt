19d129333343f5043734672a90ca5b05d2c7a152
Gogtam8 <129152884+Gogtam8@users.noreply.github.com>
2024-05-08 15:55:13 +0200

    Update and rename main.py to ToolBox.py

--- ToolBox.py
+++ ToolBox.py
@@ -1,3 +1,16 @@
+ascii_art = """
+ _______                   _   ____                 
+ |__   __|                 | | |  _ \\                
+    | |      ___     ___   | | | |_) |   ___   __  __
+    | |     / _ \\   / _ \\  | | |  _ <   / _ \\  \\ \\/ /
+    | |    | (_) | | (_) | | | | |_) | | (_) |  >  < 
+    |_|     \\___/   \\___/  |_| |____/   \\___/  /_/\\_\\
+                                                     
+     Gaspard DAVID - Théo CORBLIN - Quentin RAYMOND
+"""
+
+print(ascii_art)
+
 import subprocess
 
 def main():
@@ -14,12 +27,11 @@ def main():
             subprocess.run(["python", "test.py"])  # Exécute le script test.py
 
         elif choice == '2':
-            print("Vous avez choisi de lancer l'enumération des repertoires.")
+            print("Vous avez choisi de lancer l'enumération des répertoires.")
             subprocess.run(["python", "repertoires.py"])  # Exécute le script repertoires.py
 
         elif choice == '3':
-            print("VVous avez choisi l'option 3")
-       
+            print("Vous avez choisi l'option 3")
 
         elif choice == '4':
             print("Au revoir !")
